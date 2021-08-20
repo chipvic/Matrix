3ed0fb62b104304bf5884b19c5e9c792eaab8106
chip <huongctt3@onemount.com>
2021-08-20 09:15:20 +0700

    Bổ sung bài 4a,b,c

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,36 +1,78 @@
 import java.util.Scanner;
 
 public class App {
-public static void main(String[] args){
+    public static void main(String[] args) {
 
-   // int [][] matrix = BaiTH7.inputMatrix();
-    int[][] a = BaiTH7.generateMatrix(2,3,1,3);
-    BaiTH7.printMatrix(a);
-    int sum = BaiTH7.sumElements(a);
-    System.out.println("Sum là: "+sum);
+        System.out.println("Nhập số dòng của ma trận: ");
+        int row = BaiTH7.nhapSoInt();
+        System.out.println("Nhập số cột của ma trận: ");
+        int colunm = BaiTH7.nhapSoInt();
+        int[][] a = BaiTH7.inputMatrix(row, colunm);
 
-    // Bài 1:
-    System.out.println("Nhập số cần tìm trong Matrix: ");
-    Scanner sc = new Scanner(System.in);
-    int x = sc.nextInt();
-    if(BaiTH7.findX(a,x)){
-        System.out.format("Có giá trị %d trong mảng",x);
-    } else {
-        System.out.format("Không có giá trị %d trong mảng",x);
+        //int[][] a = BaiTH7.generateMatrix(2,3,1,99);
+
+        BaiTH7.printMatrix(a);
+        int sum = BaiTH7.sumElements(a);
+        System.out.println("Sum là: " + sum);
+
+        // Bài 1:
+        System.out.println("Nhập số cần tìm trong Matrix: ");
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        if (BaiTH7.findX(a, x)) {
+            System.out.format("Có giá trị %d trong mảng", x);
+        } else {
+            System.out.format("Không có giá trị %d trong mảng", x);
+        }
+        System.out.println();
+
+        // Bài 2:
+        if (BaiTH7.isPrimeMatrix(a)) {
+            System.out.println("Mảng toàn số nguyên tố");
+        } else {
+            System.out.println("Mảng có phần tử không phải số nguyên tố");
+        }
+
+        // Bài 3:
+        int max = BaiTH7.findMax(a);
+        System.out.format("Phần tử %d là phần tử lớn nhất của mảng", max);
+
+        // Bài 4:
+        // Câu a:
+        System.out.println("Nhập dòng cần tính tổng: ");
+        int r = BaiTH7.nhapSoInt();
+        System.out.println("Nhập cột cần tính tổng: ");
+        int c = BaiTH7.nhapSoInt();
+        if ((r > a.length) || (c > a[0].length) || (r <= 0) || (c <= 0)) {
+            System.out.println("Mảng không tồn tại cột/ dòng bạn đã nhập");
+        } else {
+            int sumRC = BaiTH7.sumRC(a, r, c);
+            System.out.format("Tổng của dòng %d và cột %d là: %d", r, c, sumRC);
+        }
+
+        // Câu b:
+        System.out.println("Nhập ma trận vuông với số dòng = cột là: ");
+        int x = BaiTH7.nhapSoInt();
+        int[][] b = BaiTH7.inputMatrix(x, x);
+        BaiTH7.printMatrix(b);
+        int sumMain = BaiTH7.sumMainDiagonal(b);
+        System.out.println("Tổng đường chéo chính là: " + sumMain);
+        int sumSub = BaiTH7.sumSubDiagonal(b);
+        System.out.println("Tổng đường chéo phụ là: " + sumSub);
+
+        // Câu c:
+        int sumUpperMain = BaiTH7.sumUpperMainDiag(b);
+        System.out.println("Tổng các phần tử phía trên đường chéo chính là: " + sumUpperMain);
+        int sumLowerMain = BaiTH7.sumLowerMainDiag(b);
+        System.out.println("Tổng các phân tử phía dưới đường chéo chính là: " + sumLowerMain);
+
+//        // Câu d:
+//        int sumUpperSub = BaiTH7.sumUpperSubDiag(b);
+//        System.out.println("Tổng các phần tử phía trên đường chéo phụ là: "+sumUpperSub);
+//        int sumLowerSub = BaiTH7.sumLowerSubDiag(b);
+//        System.out.println("Tổng các phần tử phía trên đường chéo phụ là: "+sumLowerSub);
+//
+
+
     }
-    System.out.println();
-
-    // Bài 2:
-    if(BaiTH7.isPrimeMatrix(a)){
-        System.out.println("Mảng toàn số nguyên tố");
-    } else {
-        System.out.println("Mảng có phần tử không phải số nguyên tố");
-    }
-
-    // Bài 3:
-    int max = BaiTH7.findMax(a);
-    System.out.format("Phần tử %d là phần tử lớn nhất của mảng", max);
-
-
-}
 }
--- src/main/java/BaiTH7.java
+++ src/main/java/BaiTH7.java
@@ -1,21 +1,15 @@
 import java.util.Scanner;
 
 public class BaiTH7 {
-    public static int[][] inputMatrix (){
+    public static int[][] inputMatrix(int r, int c) {
         Scanner sc = new Scanner(System.in);
-
-        System.out.println("Nhập số dòng: ");
-        int r = sc.nextInt();
-
-        System.out.println("Nhập số cột: ");
-        int c = sc.nextInt();
-
         int[][] ret = new int[r][c];
 
         for (int i = 0; i < r; i++) {
             for (int j = 0; j < c; j++) {
-                System.out.format("a[%d][%d] ",ret[i][j]);
+                System.out.format("a[%d][%d] = ", i, j);
                 ret[i][j] = sc.nextInt();
+
 
             }
 
@@ -24,21 +18,27 @@ public class BaiTH7 {
 
     }
 
-    public static void printMatrix(int[][] a){
-        for (int i = 0, r=a.length; i < r; i++) {
-            for (int j = 0, c=a[0].length; j < c; j++) {
-                System.out.format("%d ",a[i][j]);
+    public static int nhapSoInt() {
+        Scanner myInt = new Scanner(System.in);
+        int numInt = myInt.nextInt();
+        return numInt;
+    }
+
+    public static void printMatrix(int[][] a) {
+        for (int i = 0, r = a.length; i < r; i++) {
+            for (int j = 0, c = a[0].length; j < c; j++) {
+                System.out.format("%d ", a[i][j]);
             }
             System.out.println();
         }
 
     }
 
-    public static int[][] generateMatrix(int r, int c, int min, int max){
+    public static int[][] generateMatrix(int r, int c, int min, int max) {
         int[][] ret = new int[r][c];
         for (int i = 0; i < r; i++) {
             for (int j = 0; j < c; j++) {
-                ret[i][j]=(int) Math.floor(Math.random()*(max-min)+min);
+                ret[i][j] = (int) Math.floor(Math.random() * (max - min) + min);
 
             }
 
@@ -47,65 +47,56 @@ public class BaiTH7 {
 
     }
 
-    public static int sumElements(int[][] a){
-        int sum =0;
-        for (int i = 0, r=a.length ; i < r; i++) {
-            for (int j = 0, c=a[0].length; j < c; j++) {
-                sum+=a[i][j];
+    public static int sumElements(int[][] a) {
+        int sum = 0;
+        for (int i = 0, r = a.length; i < r; i++) {
+            for (int j = 0, c = a[0].length; j < c; j++) {
+                sum += a[i][j];
             }
 
         }
         return sum;
     }
 
-    public static boolean findX(int[][] a, int x){
-        boolean result = false;
-        for (int i = 0, r=a.length ; i < r; i++) {
-            for (int j = 0, c=a[0].length; j < c; j++) {
-                if (x==a[i][j]){
-                    result = true;
-                    break;
+    public static boolean findX(int[][] a, int x) {
+        for (int i = 0, r = a.length; i < r; i++) {
+            for (int j = 0, c = a[0].length; j < c; j++) {
+                if (x == a[i][j]) {
+                    return true;
                 }
             }
 
         }
-        return result;
+        return false;
     }
 
     public static boolean isPrimeNum(int n) {
-        boolean ret = true;
-        for (int i = 2; i < n; i++) {
+        for (int i = 2; i < n / 2; i++) {
             if (n % i == 0) {
-                ret = false;
-                break;
+                return false;
             }
         }
-        return ret;
+        return true;
     }
 
-    public static boolean isPrimeMatrix(int[][] a){
-        boolean ret = true;
+    public static boolean isPrimeMatrix(int[][] a) {
         for (int i = 0, r = a.length; i < r; i++) {
-            for (int j = 0, c =a[0].length; j < c; j++) {
+            for (int j = 0, c = a[0].length; j < c; j++) {
 
-                if(!isPrimeNum(a[i][j])){
-                    ret= false;
-                    break;
+                if (!isPrimeNum(a[i][j])) {
+                    return false;
                 }
-
             }
-
         }
-
-        return ret;
+        return true;
     }
 
-    public static int findMax(int[][] a){
-        int max=0;
-        for (int i = 0, r=a.length; i < r; i++) {
-            for (int j = 0, c=a[0].length; j < c; j++) {
-                if(max<a[i][j]){
-                    max=a[i][j];
+    public static int findMax(int[][] a) {
+        int max = a[0][0];
+        for (int i = 0, r = a.length; i < r; i++) {
+            for (int j = 0, c = a[0].length; j < c; j++) {
+                if (max < a[i][j]) {
+                    max = a[i][j];
                 }
 
             }
@@ -114,5 +105,105 @@ public class BaiTH7 {
 
         return max;
     }
+
+    public static int sumRC(int[][] a, int r, int c) {
+        int sum = 0;
+
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a[0].length; j++) {
+                if ((i == r - 1) || (j == c - 1)) {
+                    sum += a[i][j];
+                }
+
+            }
+
+        }
+        return sum;
+    }
+
+    public static int sumMainDiagonal(int[][] a) {
+        int sum = 0;
+        int r = a.length;
+        for (int i = 0; i < r; i++) {
+            for (int j = i; j < r; j++) {
+                sum += a[i][j];
+                break;
+            }
+
+        }
+
+        return sum;
+    }
+
+    public static int sumSubDiagonal(int[][] a) {
+        int sum = 0;
+        int r = a.length;
+        int c = a.length;
+        for (int i = 0; i < r; i++) {
+            for (int j = c - 1; j >= 0; j--) {
+                sum += a[i][j];
+                c = c - 1;
+                break;
+            }
+
+        }
+
+
+        return sum;
+    }
+
+    public static int sumUpperMainDiag(int[][] a) {
+        int sum = 0;
+        int r = a.length;
+        for (int i = 0; i < r; i++) {
+            for (int j = i + 1; j < r; j++) {
+                sum += a[i][j];
+            }
+
+        }
+
+        return sum;
+    }
+
+    public static int sumLowerMainDiag(int[][] a) {
+        int sum = 0;
+        int r = a.length;
+        for (int i = r - 1; i >= 0; i--) {
+            for (int j = i - 1; j >= 0; j--) {
+                sum += a[i][j];
+            }
+        }
+        return sum;
+    }
+
+//    public  static int sumUpperSubDiag(int[][] a){
+//        int sum=0;
+//        int r =a.length;
+//        for (int i = 0; i < r-1; i++) {
+//            for (int j =r-1; j >=i; j--) {
+//                sum+=a[i][j];
+//
+//            }
+//
+//        }
+//
+//
+//        return sum;
+//    }
+//
+//    public static int sumLowerSubDiag(int[][] a){
+//        int sum=0;
+//        int r=a.length;
+//        for (int i = r-1; i > 0; i--) {
+//            for (int j = i; j < r; j++) {
+//                sum+=a[i][j];
+//
+//            }
+//
+//        }
+//
+//        return sum;
+//    }
+
 
 }
